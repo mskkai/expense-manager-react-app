ed96a457a710c91f1adf0a9bc04c2672898d6bdd
mskkai <msasikumarbeeee@gmail.com>
2020-07-29 09:39:55 +0530

    updated files to verify heroku deployment

--- server/server.js
+++ server/server.js
@@ -3,12 +3,10 @@ const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "/public");
 const port = process.env.PORT || 3000;
+const staticServe = express.static(publicPath);
 
-app.use(express.static(publicPath));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(publicPath, "/index.html"));
-});
+app.use("/", staticServe);
+app.use("*", staticServe);
 
 app.listen(port, () => {
   console.log("server is up and running");
